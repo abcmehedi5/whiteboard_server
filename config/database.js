f2eba275e72af362f27eb276eabcda4ae0dd9824
abcmehedi5 <abcmehedi5@gmail.com>
2023-12-25 13:53:17 +0600

    fix deployment bugs

--- config/database.js
+++ config/database.js
@@ -4,12 +4,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      //   useNewUrlParser: true,
-      //   useUnifiedTopology: true,
-      //   useCreateIndex: true,
-      //   useFindAndModify: false
-    });
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mbjz2.mongodb.net/whiteboard?retryWrites=true&w=majority`,
+      {
+        //   useNewUrlParser: true,
+        //   useUnifiedTopology: true,
+        //   useCreateIndex: true,
+        //   useFindAndModify: false
+      }
+    );
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
